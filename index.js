8b17164f7811c66da4207d534ac4d52c80a5c17f
Gabriel Valle <gvalle91@gmail.com>
2018-05-25 21:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newObject;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = [value];
+  object[key] = value;
   return object;
 }
